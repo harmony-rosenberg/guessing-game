fc49d5b02974bf83db5f91005921765ca956802b
harmony <harmony.rosenberg94@gmail.com>
2023-08-29 17:58:03 -0500

    check guess function

--- user-input.js
+++ user-input.js
@@ -4,3 +4,18 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+let secretNumber = 17
+
+function checkGuess (guess) {
+    if (guess > secretNumber) {
+        console.log('Too High')
+        return false
+    } else if (guess < secretNumber) {
+        console.log("Too low")
+        return false
+    } else (num === secretNumber) {
+        console.log("Correct!")
+        return true
+    }
+}
