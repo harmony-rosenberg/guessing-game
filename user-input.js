dd19a660d8c37a5b2b44439bb23ff8c30c545a2b
Abdul Waris <abdulwarisaw532@gmail.com>
2023-08-29 19:25:40 -0400

    User Input done

--- user-input.js
+++ user-input.js
@@ -4,9 +4,10 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+// init the secret numbers.
 let secretNumber = 17
 
+// function for checking the user's guess.
 function checkGuess (guess) {
     if (guess > secretNumber) {
         console.log('Too High')
@@ -14,8 +15,25 @@ function checkGuess (guess) {
     } else if (guess < secretNumber) {
         console.log("Too low")
         return false
-    } else (num === secretNumber) {
+    } else { 
         console.log("Correct!")
         return true
+       }  
     }
-}
+
+// function to check the user's guess for the sn.
+function askGuess() {
+    rl.question('Enter a guess: ', (guess) => {
+        let numberGuess = Number(guess);
+        let result = checkGuess(numberGuess);
+
+       if (result === true) {
+        console.log('You Won!')
+        rl.close()
+       } else {
+        askGuess()
+       }
+    })
+  }
+
+askGuess()
