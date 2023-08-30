ad6e8d3188a08a02c0450143a3e59e372e4dc76a
Abdul Waris <abdulwarisaw532@gmail.com>
2023-08-29 20:24:07 -0400

    Debugged and solved

--- guessing-game.js
+++ guessing-game.js
@@ -5,57 +5,53 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// make a random whole number between min and max
-
-function randomInRange (min, max) {
-    let int =  Math.floor(Math.random() * (max - min + 1)) + min;
-    askGuess(int)
+// Generate a random whole number between min and max
+function randomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// init the secret numbers.
-let secretNumber = randomInRange(1, 100)
-
-
-// function for checking the user's guess.
-function checkGuess (guess) {
-    if (guess > secretNumber) {
-        console.log('Too High')
-        return false
-    } else if (guess < secretNumber) {
-        console.log("Too low")
-        return false
-    } else {
-        console.log("Correct!")
-        return true
-       }
-    }
-
-    function askRange() {
-        rl.question('Enter a min number: ', handleResponseOne);
-
-        function handleResponseOne(min) {
-            rl.question('Enter a max number: ', handledResponseTwo);
-
-            function handledResponseTwo(max) {
-                console.log(`I'm thinking a number between ${min} and ${max}...`);
-                randomInRange(min, max);
-            }
-        }
-    }
-
-// function to check the user's guess for the sn.
-function askGuess() {
-    rl.question('Enter a guess: ', (guess) => {
-        let numberGuess = Number(guess);
-        let result = checkGuess(numberGuess);
-
-       if (result === true) {
-        console.log('You Won!')
-        rl.close()
-       } else {
-        askGuess()
-       }
-    })
+// Function to check the user's guess
+function checkGuess(guess) {
+  if (guess > secretNumber) {
+    console.log('Too high.');
+    return false;
+  } else if (guess < secretNumber) {
+    console.log('Too low.');
+    return false;
+  } else {
+    console.log('Correct!');
+    return true;
   }
+}
 
-askRange()
+// Function to ask the user for a guess
+function askGuess() {
+  rl.question('Enter a guess: ', (guess) => {
+    const numberGuess = Number(guess);
+    const result = checkGuess(numberGuess);
+
+    if (result === true) {
+      console.log('You won!');
+      rl.close();
+    } else {
+      askGuess();
+    }
+  });
+}
+
+// Function to handle user input for the min and max range
+function askRange() {
+  rl.question('Enter a min number: ', (min) => {
+    rl.question('Enter a max number: ', (max) => {
+      console.log(`I'm thinking of a number between ${min} and ${max}...`);
+      secretNumber = randomInRange(Number(min), Number(max));
+      askGuess(); 
+    });
+  });
+}
+
+// Initialize the secret number
+let secretNumber;
+
+
+askRange();
